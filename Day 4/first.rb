389edd5a4ac020f031b46fd35750be2292ac63ac
Mike <iodiot@gmail.com>
2019-12-06 15:39:02 +0200

    Solve Day 4 and Day 5

--- Day 4/first.rb
+++ Day 4/first.rb
@@ -1,3 +1,20 @@
-File.open("input.txt", "r").each do |line|
-	p line
+from = 402328
+to = 864247
+
+n = 0
+
+for x in from..to
+	arr = x.to_s.split('').map(&:to_i)
+
+	inc = true
+	pair = false
+
+	for i in 1...6
+		inc = false if arr[i] < arr[i - 1]
+		pair = true if arr[i] == arr[i - 1]
+	end
+
+	n += 1 if inc and pair
 end
+
+puts "part 1: #{n}"
--- Day 4/second.rb
+++ Day 4/second.rb
@@ -1,3 +1,36 @@
-File.open("input.txt", "r").each do |line|
-	p line
+from = 402328
+to = 864247
+
+n = 0
+
+for x in from..to
+	arr = x.to_s.split('').map(&:to_i)
+
+	inc = true
+
+	for i in 1...6
+		inc = false if arr[i] < arr[i - 1]
+	end
+
+	next unless inc
+
+	k = 0
+
+	for i in 1...6
+		if arr[i] == arr[i - 1]
+			k += 1
+		else
+			if k == 1
+				n += 1
+				k = 0
+				break
+			end
+
+			k = 0
+		end
+	end
+
+	n += 1 if k == 1
 end
+
+puts "part 2: #{n}"
