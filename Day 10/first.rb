afa8478e537d372009572b9b618f950003080c78
Mike <iodiot@gmail.com>
2019-12-17 14:54:13 +0200

    Solve Day 14

--- Day 10/first.rb
+++ Day 10/first.rb
@@ -1,5 +1,7 @@
 # --- Day 10: Monitoring Station ---
 
+require 'ruby2d'
+
 def gcd(a, b)
 	b == 0 ? a : gcd(b, a % b)
 end
@@ -47,45 +49,61 @@ end
 # 	[asteroids.map {|a| visible?(asteroids, o[0], o[1], a[0], a[1])}.count(true) - 1, o]
 # }.max
 
-
 o = [11, 13]
 
 puts "part 1: Best is 11,13 with 210 other asteroids detected."
 
 # part 2 ----------
 
-alfa = 0
-step = Math::PI / 360
-len = 29
+def cw(a, b)
+	a[0] * b[1] - a[1] * b[0] < 0
+end
+
+def mag(v)
+	Math.sqrt(v[0] * v[0] + v[1] * v[1])
+end
+
+def angle(a, b)
+	(a[0] * b[0] + a[1] * b[1]) / (mag(a) * mag(b))
+	#Math.acos(x)
+end
+
+p angle([0, 1], [0, -1])
+
+alfa = Math::PI
 x1 = o[0]
 y1 = o[1]
 px2, py2 = -1, -1
 
 n = 0
 
-y2 = 1
+cx, cy = Window.width / 2, Window.height / 2
 
-while true 
-	# x2 = (len * Math.sin(alfa) + x1).round
-	# y2 = (-len * Math.cos(alfa) + y1).round
+p cx
 
+line = Line.new(
+  x1: cx, y1: cy,
+  width: 3,
+  color: 'lime',
+)
 
+update do
 
-	# alfa += step
+	if n < 10000
+		x2, y2 = 250 * Math.sin(-alfa), 250 * Math.cos(alfa)
 
-	# next if x2 == px2 and y2 == py2
+		line.x2 = x2 + cx
+		line.y2 = y2 + cy
 
-	# px2, py2 = x2, y2
+		#vaporize(asteroids, x1, y1, x2, y2)
 
-		x2 = 11 + n
-		y2 = -13
+		#puts "#{x2},#{y2}"
 
-vaporize(asteroids, x1, y1, x2, y2)
+		n += 1
 
+		alfa += Math::PI / 360
+	end
 
-	#puts "#{x2},#{y2}"
-#
-	n += 1
-	break if n > 10
 end
 
+show
--- Day 14/solution.rb
+++ Day 14/solution.rb
@@ -1,5 +1,52 @@
-# 
+# --- Day 14: Space Stoichiometry ---
+
+reactions = {}
 
 File.open("input.txt", "r").each do |line|
-	p line
+	left, right = line.split('=>')
+
+	out_q, out_el = right.chomp.split(' ')
+
+	reactions[out_el] = {:q => out_q.to_i, :i => left.split(',').map {|x| x.split(' ')}}
 end
+
+leftovers = {}
+
+reactions.keys.each {|x| leftovers[x] = 0}
+
+def react(reactions, leftovers, required_q, required_el)
+	available = [leftovers[required_el], required_q].min
+	leftovers[required_el] -= available
+	required_q -= available
+	return [0, 0] if required_q == 0
+
+	output_q = reactions[required_el][:q]
+
+	batches = (required_q.to_f / output_q.to_f).ceil
+
+	ore = 0
+
+	reactions[required_el][:i].each do |pair|
+		q, el  = pair[0].to_i, pair[1]
+
+		if el == 'ORE'
+			ore += q * batches
+		else
+			leftover, ore_to_add = react(reactions, leftovers, q * batches, el)
+			leftovers[el] += leftover
+			ore += ore_to_add
+		end
+	end
+
+	return [output_q * batches - required_q, ore]
+end
+
+part_1 = react(reactions, leftovers.dup, 1, 'FUEL')[1]
+
+max_ore = 1000000000000
+max_n = max_ore / 100
+
+part_2 = (1..max_n).bsearch {|x| react(reactions, leftovers.dup, x, 'FUEL')[1] > max_ore } - 1
+
+puts "part 1: #{part_1}"
+puts "part 2: #{part_2}"
